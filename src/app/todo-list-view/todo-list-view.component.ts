5450106036d37d4a86d3cd04749c92bf6108b841
Tadiwanashe Shangwa <tshxng@gmail.com>
2024-04-09 17:10:58 +0200

    adds list view component

---
+++ src/app/todo-list-view/todo-list-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-list-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './todo-list-view.component.html',
+  styleUrl: './todo-list-view.component.css'
+})
+export class TodoListViewComponent {
+
+}
