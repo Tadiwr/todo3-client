91005690abfe9510e2def43f70748c9e2e486d35
Tadiwanashe Shangwa <tshxng@gmail.com>
2024-04-09 17:11:24 +0200

    adds home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { TodoListViewComponent } from "../todo-list-view/todo-list-view.component";
+import { BottomBarComponent } from "../bottom-bar/bottom-bar.component";
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+    selector: 'app-home',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.css',
+    imports: [TodoListViewComponent, BottomBarComponent]
 })
 export class HomeComponent {
 
