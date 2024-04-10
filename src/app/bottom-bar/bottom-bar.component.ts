52a18615c00fa1a737f5c420421c0ff3a2ff3acd
Tadiwanashe Shangwa <tshxng@gmail.com>
2024-04-10 15:17:23 +0200

    adds input and on click functions on the bottom component

--- src/app/bottom-bar/bottom-bar.component.ts
+++ src/app/bottom-bar/bottom-bar.component.ts
@@ -1,12 +1,29 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-bottom-bar',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './bottom-bar.component.html',
-  styleUrl: './bottom-bar.component.css'
+  styleUrl: './bottom-bar.component.css',
+  providers : [HttpClientModule]
+  
 })
 export class BottomBarComponent {
+  constructor(private http: HttpClient) {}
 
+  newTodo = '';
+
+  private api_url = "http://localhost:8080/todos";
+
+  addTodo() {
+    console.log(this.newTodo);
+    this.newTodo = '';
+  }
+
+  onInput() {
+    console.log("Value changed")
+  }
 }
